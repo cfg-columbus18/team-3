1be85c8acb3ba793bbfa6cba39a70ce7ede4c1a3
elizabethshulman <ers58@duke.edu>
2018-10-20 02:23:57 -0400

    Moves Greeting class to `model` package

--- cfg-team3/src/main/java/com/team3/cfgteam3/controller/GreetingController.java
+++ cfg-team3/src/main/java/com/team3/cfgteam3/controller/GreetingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.team3.cfgteam3.Greeting;
+import com.team3.cfgteam3.model.Greeting;
 
 @RestController
 public class GreetingController {
