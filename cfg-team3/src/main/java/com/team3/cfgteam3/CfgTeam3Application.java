3ec970454369ceda4fe813644478bcd57c1a31fd
Kyle Harvey <kph18@duke.edu>
2018-10-20 00:50:41 -0400

    Dbcommit

--- cfg-team3/src/main/java/com/team3/cfgteam3/CfgTeam3Application.java
+++ cfg-team3/src/main/java/com/team3/cfgteam3/CfgTeam3Application.java
@@ -1,5 +1,8 @@
 package com.team3.cfgteam3;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +11,34 @@ public class CfgTeam3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CfgTeam3Application.class, args);
+		Connection conn = null;
+		try {
+	     conn = DBClass.connectToH2();
+		
+		Statement stmt = conn.createStatement();
+		
+		String sqlString = "CREATE TABLE IF NOT EXISTS Activity "
+				+ "(Name VARCHAR(1000) NOT NULL PRIMARY KEY, "
+				+ "Type VARCHAR(256) NOT NULL,"
+				+ "Last timestamp NOT NULL,"
+				+ "Total INTEGER(1000) DEFAULT 0,"
+				+ "Description VARCHAR(10000));";
+		stmt.execute(sqlString);
+//		Statement stmt2 = conn.createStatement();
+//		String sqlTrig = "CREATE TRIGGER updateTrigger "
+//				+ "BEFORE UPDATE ON Activity "
+//				+ "IF (OLD.last > NEW.last) THEN "
+//				+ "NEW = OLD "
+//				+ "ELSE "
+//				+ "NEW.Total = OLD.Total +1 "
+//				+ "END IF "
+//				+ "NEW;";
+//		stmt2.execute(sqlTrig);
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		} finally {
+			DBClass.close(conn);
+		}
+		
 	}
 }
